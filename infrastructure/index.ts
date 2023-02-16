ad3f5f5535bd7c9a8499b5b75371cf2c41ef7436
Baran Karaaslan <barankaraaslan@protonmail.com>
2023-02-16 23:20:17 +0300

    add certificate for better domain name

--- infrastructure/index.ts
+++ infrastructure/index.ts
@@ -6,11 +6,20 @@ import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
 import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
 import * as cloudfrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
+import * as route53 from "aws-cdk-lib/aws-route53";
+import * as acm from "aws-cdk-lib/aws-certificatemanager";
 import { join } from "path";
 
 export class MainStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const hostedZone = route53.HostedZone.fromLookup(this, "hostedzone", {
+      domainName: "tinasour.click",
+    });
+    new acm.Certificate(this, "mySiteCert", {
+      domainName: `cv.${hostedZone.zoneName}`,
+      validation: acm.CertificateValidation.fromDns(hostedZone),
+    });
     const bucket = new s3.Bucket(this, "bucket", {
       bucketName: `${this.account}-${this.region}-cv`,
     });
@@ -31,4 +40,9 @@ export class MainStack extends cdk.Stack {
 }
 
 const app = new cdk.App();
-new MainStack(app, "cv", {});
+new MainStack(app, "cv", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
