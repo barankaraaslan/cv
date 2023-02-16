fa4eadf3c2d38b2748c5c6ff308e2bd1066ce8d8
Baran Karaaslan <barankaraaslan@protonmail.com>
2023-02-16 21:34:52 +0300

    add distribution

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -2,6 +2,8 @@ import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
 import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import * as cloudfront from "aws-cdk-lib/aws-cloudfront";
+import * as cloudfrontOrigins from "aws-cdk-lib/aws-cloudfront-origins";
 import { join } from "path";
 
 export class InfrastructureStack extends cdk.Stack {
@@ -10,13 +12,18 @@ export class InfrastructureStack extends cdk.Stack {
     const bucket = new s3.Bucket(this, "bucket", {
       bucketName: `${this.account}-${this.region}-cv`,
     });
+    const cvFilename = "cv.pdf";
     new s3Deployment.BucketDeployment(this, "deployment", {
       destinationBucket: bucket,
       sources: [
         s3Deployment.Source.asset(join(__dirname, "../.."), {
-          exclude: ["**", "!cv.pdf"],
+          exclude: ["**", `!${cvFilename}`],
         }),
       ],
+    });
+    new cloudfront.Distribution(this, "dist", {
+      defaultBehavior: { origin: new cloudfrontOrigins.S3Origin(bucket) },
+      defaultRootObject: cvFilename,
     });
   }
 }
