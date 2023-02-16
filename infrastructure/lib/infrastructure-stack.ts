408df12223edbda956a37ab5dc7973aea6d087ec
Baran Karaaslan <barankaraaslan@protonmail.com>
2023-02-16 21:12:25 +0300

    add bucket

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -1,12 +1,22 @@
 import * as cdk from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as s3Deployment from "aws-cdk-lib/aws-s3-deployment";
+import { join } from "path";
 
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    new s3.Bucket(this, "bucket", {
+    const bucket = new s3.Bucket(this, "bucket", {
       bucketName: `${this.account}-${this.region}-cv`,
+    });
+    new s3Deployment.BucketDeployment(this, "deployment", {
+      destinationBucket: bucket,
+      sources: [
+        s3Deployment.Source.asset(join(__dirname, "../.."), {
+          exclude: ["**", "!cv.pdf"],
+        }),
+      ],
     });
   }
 }
